91c29ecd9d73e4b79531e630d274b6529ee2972d
sfreifeld <sfrei.1996@gmail.com>
2023-08-17 20:38:05 -0600

    got contentful to work!

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,3 +1,34 @@
+var contentful = require('contentful');
+
+
+var client = contentful.createClient({
+  space: '2z4ndwjxg4ws',
+  accessToken: 'ATS5PBZP_UXQgvg5lODlPfu_1gpDudoxeiu58XrLEMM',
+});
+
+var contentContainer = document.getElementById('contentful-example');
+
+client.getEntries()
+  .then(function (response) {
+    response.items.forEach(function (entry) {
+      var entryTitle = entry.fields.articleName;
+      var entryDiv = document.createElement('div');
+      entryDiv.className = 'class';
+      entryDiv.innerHTML = '<h2 class="text-text">' + entryTitle + '</h2>';
+      contentContainer.appendChild(entryDiv);
+    }
+    );
+
+
+})
+.catch(console.error);
+
+
+
+
+
+
+
 jQuery( document ).ready(function() {
 
     $(window).scroll(function(){
